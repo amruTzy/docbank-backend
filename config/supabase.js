95a8786768e82ef1fba5ab5abf687600561b8eaa
MuhamadTegarAmru <117263639+amruTzy@users.noreply.github.com>
2025-11-05 21:50:59 +0700

    Add files via upload

---
+++ config/supabase.js
@@ -0,0 +1,9 @@
+// config/supabase.js
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY // pakai service role key
+);
+
+module.exports = supabase;
